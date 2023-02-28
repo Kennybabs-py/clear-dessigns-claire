cd22d32c5dd4de213f46490a71fdc45a6ba20b0c
Kennybabs-py <babalolakehinde74@gmail.com>
2023-02-28 10:19:19 +0100

    pkg: added sass pkg

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,18 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import Head from "next/head";
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>Clear Designs by Claire</title>
+        <meta name="description" content="Clear Designs By Claire" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <Component {...pageProps} />
+    </>
+  );
 }
