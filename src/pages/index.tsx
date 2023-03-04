e3a8dbabf9cba0e991b446b98b4f3e86ecbbc4c2
Kennybabs-py <babalolakehinde74@gmail.com>
2023-03-04 23:55:57 +0100

    updated home/view-works section; added imgs to assets

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,7 +6,7 @@ export default function Home() {
     <>
       <HomeHero />
       <Works />
-      {/* <ViewWorks /> */}
+      <ViewWorks />
     </>
   );
 }
--- src/screens/home/ViewWorks/index.tsx
+++ src/screens/home/ViewWorks/index.tsx
@@ -1,10 +1,12 @@
 import React from "react";
-import TextImageWrap from "@/components/TextImageWrap";
+import styles from "./styles.module.scss";
 
 export default function ViewWorks() {
   return (
-    <section>
-      <TextImageWrap heading="Functional and Aesthetic Design Services" />
+    <section className={styles.view__works}>
+      <h2 className={styles.view__works__heading}>
+        view our <span>works</span>
+      </h2>
     </section>
   );
 }
